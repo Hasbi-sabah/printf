0eb7d0a579e86582bab3d8c82a443a985541c8d1
H. Sabah <sabahhasbi00@gmail.com>
2023-04-22 16:36:06 -0700

    if buff

--- _printf.c
+++ _printf.c
@@ -32,8 +32,8 @@ int _printf(const char *format, ...)
 	va_start(conv, format);
 	i = call_funcs(conversion, conv, format, buff);
 	va_end(conv);
-	buff[i] = '\0';
-	write(1, buff, i);
+	if (*buff)
+		write(1, buff, i);
 	return (i);
 }
 /**
